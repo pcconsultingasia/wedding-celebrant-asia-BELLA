688dc21cb2fa34acf7dc298169823c86f4fa5d99
pccunny <paul@pcconsultingasia.com>
2019-10-20 17:07:40 +0700

    modified:   src/pages/404.jsx

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 const NotFoundPage = () => (
   <Layout>
     <h1>NOT FOUND</h1>
-    <p><a href= "https://weddingcelebrantasia">You just hit an old route that doesn&#39;t exist anymore. Please return to my main page for the latest Celebrant News - Many thanks Paul</a></p>
+    <p><a href= "https://weddingcelebrantasia.com">You just hit an old route that doesn&#39;t exist anymore. Please return to my main page for the latest Celebrant News - Many thanks Paul</a></p>
   </Layout>
 );
 
